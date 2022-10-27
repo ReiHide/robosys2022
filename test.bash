e6cc62a198b75cad6373cdf824f6a08675146499
Hide Rei <s21C1139wv@s.chibakoudai.jp>
2022-10-27 12:30:53 +0900

    change command

--- test.bash
+++ test.bash
@@ -1,5 +1,16 @@
 #!/bin/bash
+# SPDX-FileCopy rightText: 2022 Hideya Reizen
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
 
 out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
 
-[ "${out}" = 14 ]
+[ "$res" = 0 ] && echo OK
+exit $res
