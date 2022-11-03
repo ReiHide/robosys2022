ff899a41b4e173cf7197dd2bb537bde82bec1570
Hide Rei <s21C1139wv@s.chibakoudai.jp>
2022-11-03 12:13:27 +0900

    over heaven

--- test.bash
+++ test.bash
@@ -12,14 +12,13 @@ res=0
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
 
-
 out=$(echo あ | ./plus)
 [ "$?" = 1 ]      || ng ${LINENO}
-[ "${put}" = "" ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 out=$(echo | ./plus)
 [ "$?" = 1 ]      || ng ${LINENO}
-[ "${put}" = "" ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
-exit $res
+ exit $res
